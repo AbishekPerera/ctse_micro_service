753523b425b888b0b68b12a46e63955fe17a4720
sahan.perera <sahan.perera@rootcodelabs.com>
2024-04-17 17:46:56 +0530

    feat: added code changes related to JWT token generation and validation and implemented an endpoint for the authentication

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import productRouter from './api/product/product.router';
 import categoryRouter from './api/category/category.router';
 import errorHandler from './middleware/errorMiddleware';
 import authMiddleware from './middleware/authMiddleware';
+import authRouter from './api/auth/auth.router';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+
+app.use('/api/auth', authRouter);
 app.use(authMiddleware);
 app.use(errorHandler);
 
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,19 +1,20 @@
+import jwt from 'jsonwebtoken';
+
 function authMiddleware(req, res, next) {
     const authHeader = req.headers.authorization;
-
     if (!authHeader) {
         return res.status(401).json({ message: 'Authorization required' });
     }
 
     const token = authHeader.split(' ')[1];
-
-    if (token !== process.env.AUTH_TOKEN) {
+    try {
+        jwt.verify(token, process.env.JWT_SECRET);
+        next();
+    } catch (error) {
         return res
             .status(401)
-            .json({ message: 'Invalid authentication token' });
+            .json({ message: 'Invalid authentication token: ' + error.message });
     }
-
-    next();
 }
 
 export default authMiddleware;
